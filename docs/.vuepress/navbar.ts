263397cd7583196c5f3383a9e7ab174d1563df0b
siqiuchen <Siqiu.Chen@dentsplysirona.com>
2022-08-27 21:09:31 +0800

    删除导航

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,5 +3,5 @@ import { navbar } from "vuepress-theme-hope";
 // 精选图标：https://vuepress-theme-hope.github.io/v2/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default navbar([
   { text: "博客", icon: "blog", link: "/blog" },
-  { text: "工具收藏", icon: "tool", link: "https://nav.newzone.top/" },
+  // { text: "工具收藏", icon: "tool", link: "https://nav.newzone.top/" },
 ]);
