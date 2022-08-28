dbeec787b9684e5cb43fac02fd8820df722b8908
siqiuchen <Siqiu.Chen@dentsplysirona.com>
2022-08-28 14:36:09 +0800

    调整icon

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,7 +3,7 @@ import {navbar} from "vuepress-theme-hope";
 // 精选图标：https://vuepress-theme-hope.github.io/v2/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default navbar([
     {text: "博客", icon: "blog", link: "/blog"},
-    {text: "leetcode", icon: "blog", link: "/leetcode/",},
+    {text: "leetcode", icon: "blog", link: "/leetcode/"},
     // {text: "代码", icon: "blog", prefix: "/code/", link: "", collapsable: true, children: "structure"},
     // {text: "devops", icon: "blog", prefix: "/deploy/", link: "", collapsable: true, children: "structure"},
     // {text: "计划", icon: "blog", prefix: "/plans/", link: "", collapsable: true, children: "structure"},
