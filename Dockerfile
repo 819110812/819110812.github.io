c2a1b66c58a0326e53a6be771d8dc3a0c9cc84e4
siqiuchen <Siqiu.Chen@dentsplysirona.com>
2022-08-28 14:24:15 +0800

    调整页面

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM blasteh/vuepress
+
+WORKDIR /app
+
+COPY . .
+
+RUN npm config set registry https://registry.npmmirror.com/ && \
+    npm i pnpm -g && \
+    pnpm add vuepress@next vuepress-theme-hope@next @vuepress/plugin-search@next && pnpm i && pnpm up
+
+EXPOSE 8080
+
+ENTRYPOINT ["pnpm","docs:dev"]
--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,7 +1,13 @@
-import { navbar } from "vuepress-theme-hope";
+import {navbar} from "vuepress-theme-hope";
 
 // 精选图标：https://vuepress-theme-hope.github.io/v2/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default navbar([
-  { text: "博客", icon: "blog", link: "/blog" },
-  // { text: "工具收藏", icon: "tool", link: "https://nav.newzone.top/" },
+    {text: "博客", icon: "blog", link: "/blog"},
+    {text: "leetcode", icon: "", prefix: "/leetcode/", link: "", collapsable: true, children: "structure"},
+    {text: "代码", icon: "", prefix: "/code/", link: "", collapsable: true, children: "structure"},
+    {text: "devops", icon: "", prefix: "/deploy/", link: "", collapsable: true, children: "structure"},
+    {text: "计划", icon: "", prefix: "/plans/", link: "", collapsable: true, children: "structure"},
+    {text: "软件架构", icon: "", prefix: "/arch/", link: "", collapsable: true, children: "structure"},
+    {text: "阅读", icon: "", prefix: "/Read/", link: "", collapsable: true, children: "structure"},
+    // { text: "工具收藏", icon: "tool", link: "https://nav.newzone.top/" },
 ]);
