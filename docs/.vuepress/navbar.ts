b931820194c8f9f761aedd50bf0c949bde9b5456
siqiuchen <Siqiu.Chen@dentsplysirona.com>
2022-09-05 20:34:51 +0800

    [java] 增加java栏目

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -3,10 +3,6 @@ import {navbar} from "vuepress-theme-hope";
 // 精选图标：https://vuepress-theme-hope.github.io/v2/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default navbar([
     {text: "首页", link: "/home", icon: "icon-shouye"},
-    {text: "阅读", icon: "icon-yuedu", link: "/read"},
-    {text: "代码", icon: "icon-daima", children: [
-            "/code/Regex.md",
-        ]},
     {text: "计划", icon: "plan", link: "/plans/TodoList"},
     {text: "工具", icon: "tool", link: "/tools"},
     {
@@ -29,6 +25,13 @@ export default navbar([
         children: [
             "/acp/弹性伸缩.md"
         ]
+    },
+    {
+        text: "java",
+        icon: "java",
+        children: [
+            "/java/springboot笔记01.md",
+            ]
     }
     // {text: "代码", icon: "blog", prefix: "/code/", link: "", collapsable: true, children: "structure"},
     // {text: "devops", icon: "blog", prefix: "/deploy/", link: "", collapsable: true, children: "structure"},
---
+++ docs/java/springboot笔记01.md
@@ -0,0 +1,11 @@
+---
+article: true
+title: springboot笔记01 - SpringFactoriesLoader
+tag:
+- springboot
+order: 1
+---
+
+# springboot笔记01 - SpringFactoriesLoader
+
+
