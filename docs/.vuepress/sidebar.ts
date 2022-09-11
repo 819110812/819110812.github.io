5af92d15907746097d06d8dc231ba1107c28078f
siqiuchen <Siqiu.Chen@dentsplysirona.com>
2022-09-11 11:24:00 +0800

    [config] add 目录

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -2,14 +2,14 @@ import {sidebar} from "vuepress-theme-hope";
 
 // 精选图标：https://vuepress-theme-hope.github.io/v2/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default sidebar([
-    {
-        text: "博客文章",
-        icon: "blog",
-        prefix: "/_posts/",
-        link: "/blog",
-        collapsable: true,
-        children: "structure",
-    },
+    // {
+    //     text: "博客文章",
+    //     icon: "blog",
+    //     prefix: "/_posts/",
+    //     link: "/blog",
+    //     collapsable: true,
+    //     children: "structure",
+    // },
     {
         text: "阅读",
         icon: "",
