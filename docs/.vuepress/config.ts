15c16dce425da28d9b1094c8f9468612d213df3b
siqiuchen <Siqiu.Chen@dentsplysirona.com>
2022-10-17 22:29:37 +0800

    [config]优化SEO

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -33,9 +33,10 @@ export default defineUserConfig({
         [
             "script",
             {
-                "data-ad-client": "ca-pub-7103594650498826",
+                // "data-ad-client": "ca-pub-7103594650498826",
                 async: true,
-                src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7103594650498826",
+                crossorigin: "anonymous",
             }
         ],
     ],
